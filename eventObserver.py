c37bafeead3fc3f87054c0a0529dd59724c74289
tcornall <terry.cornall@synchrotron.org.au>
2020-09-15 14:27:39 +1000

    Fixed WentTrueEvent and tested it in the demo loop
    A development on the SimpleEvent so that it only returns True if the objects value has only just changed to True. Good for buttons and such

--- eventObserver.py
+++ eventObserver.py
@@ -11,7 +11,7 @@ extra args and kwargs can be passed to the class at instantiation that get passe
 #The event contains an object that has a value attr, a checkFunc, value, lastValue and timestamp
 #that allows it to decide if it has happened, when and why
 class SimpleEvent:   # a base event type with a simple checFunc
-    def __init__(self,obj, name='simpleEvent'): #needs an object with a value attr and a function to test it with
+    def __init__(self,obj=None, name='simpleEvent'): #needs an object with a value attr and a function to test it with
         self.name = name
         self.value = 0 #the value that caused the event
         self.obj = obj #obj must have an attribute of 'value' or it will cause issues.
@@ -23,10 +23,53 @@ class SimpleEvent:   # a base event type with a simple checFunc
         update = getattr(self.obj, "update", None)  #like a Debouncer has an update function that needs to be called
         if callable(update):
             update()    #need to do this e.g. for a debounced pin.
-        self.lastValue = self.value #remeber what it was
         self.value = self.obj.value #and then get a new value from the obj
         return self.obj.value   #just use the event object's value as the check
 
+"""
+TimerEvent is an the event where the thing to check for is elapsed time. It repeats by default, i.e is periodic
+period is in ms
+Based on SimpleEvent and overrides the checkFunc()
+"""
+class TimerEvent(SimpleEvent):
+    def __init__(self, obj=None, name='simpleEvent', period=1000, *args, **kwargs): #have a default period of 1000ms
+        self.period = period #period is in ms
+        #print(self.period)
+        super().__init__(obj=obj, name=name, *args, **kwargs)
+        self.startTime = time.monotonic() * 1000 #remember when you were started
+
+    def checkFunc(self):  #
+        now = time.monotonic() * 1000
+        elapsedTime = now - self.startTime
+        self.value = elapsedTime
+        if elapsedTime >= self.period: #time elapsed yet?
+            self.startTime = now
+            return True  #returning True triggers the notification of the observers by the observable
+        else:
+            return False
+
+"""
+WentTrueEvent where the event is an object with a 'value' attribute.
+The event remembers the last value of the object and compares to the current one
+and if it has just changed from False to True its observable then notifies its observers
+If the object is an instance of Debouncer it also does the Debouncer.update()
+"""
+class WentTrueEvent(SimpleEvent):
+    def __init__(self, obj=None, name='wentTrueEvent', *args, **kwargs):
+        super().__init__(obj=obj, name=name, *args, **kwargs)
+
+    def checkFunc(self):  #
+        super().checkFunc()  #call the base checkFunc to get objects value and last value
+        if self.value and not self.lastValue: #was it False and just now turned True?
+            return True  #
+        else:
+            return False
+
+
+"""
+Observable is a class that takes an event and calls its checkFunc and then notifies its observers
+which register/deregister with it.
+"""
 class Observable:
     def __init__(self, name, event, *args, **kwargs):
         self._observers = []
@@ -40,6 +83,7 @@ class Observable:
     #check() calls events checkFunc to decide if time for notifications.
     #When using this observable in an eventloop, just call its check()
     def check(self):
+        self.event.lastValue = self.event.value #remember what it was
         res = self.event.checkFunc()  #remember the result, not just true or false
         if res:  #if the event has happened
             self.event.timestamp = time.monotonic()
@@ -59,80 +103,53 @@ class Observable:
         for observer in self._observers:
             observer.notify(self.name, self.event,**self.kwargs) #send the name and the repacked args and kwargs
 
-#make an Observable where the event is elapsed time. It repeats by default, i.e is periodic
-#period is in ms and has been tested down to 1 ms OK.
-class TimerObservable(Observable):
-    def __init__(self, name, *args, **kwargs):
-        self.period = kwargs.pop('period') #period is in ms
-        #print(self.period)
-        super().__init__(name, *args, **kwargs)
-        self.startTime = time.monotonic() * 1000 #remember when you were started
 
-    def check(self):  #
-        now = time.monotonic() * 1000
-        el = now - self.startTime
-        if el >= self.period: #time elapsed yet?
-            self.startTime = now
-            self.notify(res=el)  #Tell all your observers how much time actually elapsed
-            return True
-        else:
-            return False
-
-"""
-make an Observable where the event is an object with a 'value' attribute.
-i.e. it remembers the last input and compares the current one
-and check() is true of input.value has just changed from False to True
-If the object is an instance of Debouncer it also does the Debouncer.update()
-"""
-class WentTrueObservable(Observable):
-    def __init__(self, name, inp=None, *args, **kwargs):
-        super().__init__(name, *args, **kwargs)
-        self.inp = inp #period is in ms
-        print(self.name, 'inp=', self.inp)
-        if inp is not None:
-            self.lastPin = inp.value #remember what the pin was
-
-    def check(self):  #
-        if self.inp is not None:
-            if isinstance(self.inp, Debouncer):
-                self.inp.update()    #need to do this for a debounced pin.
-            inpV = self.inp.value #read the pin
-            res = inpV and not self.lastPin  #in the pin now True but wasn't last time?
-            self.lastPin = inpV #remember for next time
-            if res:
-                self.notify(res=inpV)  #Tell all your observers
-                return True    #return result
-            else:
-                return False
-        else:
-            return False #if no pin, just say 'No'
 
 class Observer:
-    def __init__(self, name, observable):
+    def __init__(self, name, observable, *args, **kwargs):
         observable.register(self)
         self.name = name
+        self.args = args
+        self.kwargs=kwargs
+        if 'led' in self.kwargs: #was there an indicator led given to the observable? Wonder if this should be in the event instead?
+            self.led = kwargs.pop('led')
+        else: self.led=None
     def notify(self,observableName, event, **kwargs): #args and kwargs also
         print(self.name, 'Got event', event.name, 'value=', event.value,'lastValue=', event.lastValue, 'at time', event.timestamp, 'kwargs=', kwargs, 'From', observableName)
-        if 'led' in kwargs: #was there an indicator led given to the observable? Wonder if this should be in the event instead?
-            led = kwargs.pop('led')
-            led.value = not led.value #toggle led to indicate observer has been notifed
+        if self.led: #was there an indicator led given to the observer
+            self.led.value = not self.led.value #toggle led to indicate observer has been notifed
 
 ############################## main #########################
 def demoLoop():
-    led = digitalio.DigitalInOut(board.D13)
-    led.direction = digitalio.Direction.OUTPUT
+#indicators
+    redLed = digitalio.DigitalInOut(board.D13)
+    redLed.direction = digitalio.Direction.OUTPUT
+
+#objects that cause events
     touchA0 = touchio.TouchIn(board.A0)  #built-in capacitive sensor. needs no external h/w except 1MOhm pulldown
     touchSwA0=Debouncer(touchA0)
-    simpleEvent = SimpleEvent(obj=touchSwA0) #Watch the touch sw on A0 and report if it goes True
 
-    testSimpleEventObservable = Observable(name='simpleEventObserver', event=simpleEvent, led=led)
+#Events that determine if the objects they monitor hae done something interesting
+    simpleEvent = SimpleEvent(obj=touchSwA0) #Watch the touch sw on A0 and report if it IS True
+    timerEvent = TimerEvent(name='timerEvent', period=3000)  #this one doesn't have an 'obj' as it monitors time
+    wentTrueEvent = WentTrueEvent(obj=touchSwA0, name='wentTrueEvent') #Watch the touch sw on A0 and report if it GOES True
 
-    observer1 = Observer('observer1', testSimpleEventObservable)
+#Observables that monitor events and notify registered observers
+    testSimpleEventObservable = Observable(name='testSimpleEventObservable', event=simpleEvent)
+    testTimerEventObservable = Observable(name='testTimerEventObservable', event=timerEvent)
+    testWentTrueObservable = Observable(name='testWentTrueObservable', event=wentTrueEvent)
 
-    observerableList = [testSimpleEventObservable]
+#observers that get notifications from observables and do something about it
+    simpleObserver = Observer('simpleObserver', testSimpleEventObservable, led=redLed) #who the observable should notify if event happens
+    timerObserver = Observer('timerObserver', testTimerEventObservable, led=redLed)
+    wentTrueObserver = Observer('wentTrueObserver', testWentTrueObservable, testWentTrueObservable, led=redLed)
 
-    #The forever loop. Causes observable to check their events and then notify their observers.
+#list of observables to be checked in the 'forever' loop
+    #observerableList = [testSimpleEventObservable, testTimerEventObservable, testWentTrueObservable]
+    observerableList = [testWentTrueObservable]
+
+#The forever loop. Causes observable to check their events and then notify their observers.
     while True:
         for obs in observerableList:
             res=obs.check() #ask this observable to check it event. This will cause a notify if it has happened
-            time.sleep(0.1) #need this if you are calling print
+        #time.sleep(0.1) #might need this if you are calling print every time else it stalls Mu
